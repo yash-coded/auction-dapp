7faf304d48b6e1ead4ea491b588771b565fa64d2
Yash Patel <patelyash3377@gmail.com>
2025-01-02 16:12:45 -0700

    Add sepolia banner

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,6 +11,8 @@ import {
 } from "./ui/dropdown-menu";
 import { useSDK } from "@metamask/sdk-react";
 import { ConnectWalletButton } from "./ConnectWalletButton";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { AlertCircle } from "lucide-react";
 
 export const Navbar = () => {
   const { account, sdk } = useSDK();
@@ -21,36 +23,48 @@ export const Navbar = () => {
     }
   };
   return (
-    <div className="md:flex md:flex-row flex flex-col md:justify-between md:items-center md:space-y-0 space-y-4">
-      <Link href={"/"}>
-        <h1 className="lg:text-4xl  md:text-2xl text-lg font-extrabold cursor-pointer">
-          Decentralized Auction
-        </h1>
-      </Link>
-      <div className="flex space-x-4">
-        <ConnectWalletButton />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Avatar className="cursor-pointer hover:bg-slate-500">
-              <AvatarFallback>A</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="mr-10 min-w-52">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <Link href="/createAuction">
-              <DropdownMenuItem>Create Auction</DropdownMenuItem>
-            </Link>
-            <Link href={`/myAuctions`}>
-              <DropdownMenuItem>My Auctions</DropdownMenuItem>
-            </Link>
-            {account && (
-              <DropdownMenuItem onClick={disconnect}>
-                <p className="text-red-600">Disconnect Wallet</p>
-              </DropdownMenuItem>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <div>
+      <div className="md:flex md:flex-row flex flex-col md:justify-between md:items-center md:space-y-0 space-y-4">
+        <Link href={"/"}>
+          <h1 className="lg:text-4xl  md:text-2xl text-lg font-extrabold cursor-pointer">
+            Decentralized Auction
+          </h1>
+        </Link>
+        <div className="flex space-x-4">
+          <ConnectWalletButton />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="cursor-pointer hover:bg-slate-500">
+                <AvatarFallback>A</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mr-10 min-w-52">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <Link href="/createAuction">
+                <DropdownMenuItem>Create Auction</DropdownMenuItem>
+              </Link>
+              <Link href={`/myAuctions`}>
+                <DropdownMenuItem>My Auctions</DropdownMenuItem>
+              </Link>
+              {account && (
+                <DropdownMenuItem onClick={disconnect}>
+                  <p className="text-red-600">Disconnect Wallet</p>
+                </DropdownMenuItem>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+      <div className="mt-6">
+        <Alert className="border border-yellow-500 bg-yellow-100 text-yellow-900">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Sepolia network</AlertTitle>
+          <AlertDescription>
+            This app uses Sepolia network. Please do not use real assets to
+            interact with it
+          </AlertDescription>
+        </Alert>
       </div>
     </div>
   );
